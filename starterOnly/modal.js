9e97ac7c6cc0949b3ab0ef9222fae79ea56cc5f3
Aba <sarah.aba@hotmail.fr>
2024-03-11 19:29:17 +0100

    modification fichier assets/ modification css/ description commentaire code js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,6 +1,6 @@
-//ajoute une la classe responsive au elements  de myTopnav
+// Ajoute la classe "responsive" aux éléments de "myTopnav"
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  const x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -8,7 +8,7 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// Sélection des classes et des ID nécessaires
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
@@ -16,43 +16,41 @@ const btnFermer = document.querySelector(".close");
 const btnValider = document.getElementById("btn-valider");
 const modalContent = document.getElementById("modal-content");
 const modalFinFormulaire = document.querySelector(".modal-fin-formulaire");
-const inputFermerFormulaire = document.getElementById("input-fermer-formulaire");
+const inputFermerFormulaire = document.getElementById(
+  "input-fermer-formulaire"
+);
 const body = document.querySelector("body");
 
-
-
-// launch modal event
+// Écoute du clic sur les boutons "Je m'inscris" et déclenchement de la fonction "launchModal"
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
-function launchModal() {
-showModal(true)
-
-}
-//launch modal btn fermer
-btnFermer.addEventListener("click", () => {
-showModal(false)
-});
-
-// input fermer formulaire
-inputFermerFormulaire.addEventListener("click", () => {
- showModal(false)
-});
-
+// Fonction qui modifie les styles CSS pour afficher ou masquer la modal
 function showModal(bool) {
   if (bool) {
     modalbg.style.display = "block";
-   
   } else {
     modalbg.style.display = "none";
-    
   }
 }
 
+// Fonction "launchModal" appelant la fonction "showModal" pour afficher ou masquer la modal
+function launchModal() {
+  // Modification des styles CSS pour rendre la modal visible
+  showModal(true);
+}
 
-//launch modal btn formulaire valide
+// Écoute du clic sur le bouton "fermer" de la modal
+btnFermer.addEventListener("click", () => {
+  // Appel de la fonction pour rendre la modal non visible
+  showModal(false);
+});
 
-// Soumission formulaire
+// Écoute du clic sur le bouton de fermeture de la fin du formulaire
+inputFermerFormulaire.addEventListener("click", () => {
+  showModal(false);
+});
+
+// Soumission du formulaire : récupération de la classe du formulaire, si soumis, appel de la fonction "validate"
 document
   .querySelector('form[name="reserve"]')
   .addEventListener("submit", (event) => {
@@ -60,111 +58,110 @@ document
     validate();
   });
 
-//function avec 2 arguments creer span et ne pas afficher plusieurs fois msg erreur
+// Fonction qui crée un élément "span" pour afficher un message d'erreur spécifique et éviter les doublons
 function createSpan(errorMessage, id) {
   const parentElement = document.getElementById(id);
   document.getElementById(id).innerHTML = "";
   const span = document.createElement("span");
   span.textContent = errorMessage;
-  //creation de la classe pour les elements span
   span.classList.add("error-message");
   parentElement.appendChild(span);
 }
 
-//validation email
+// Validation de l'adresse email
 function validateEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
 }
 
-
-//function validation formulaire
+// Fonction de validation du formulaire
 function validate() {
-  //recuperation des ID du formulaire
+  // Récupération des valeurs des champs du formulaire
   const firstName = document.getElementById("first").value;
   const lastName = document.getElementById("last").value;
   const email = document.getElementById("email").value;
   const birthdate = document.getElementById("birthdate").value;
   const quantity = document.getElementById("quantity").value;
   const villes = document.querySelectorAll(".verification-ville");
-const condition = document.getElementById("checkbox1");
+  const condition = document.getElementById("checkbox1");
 
-
-
-  //verification validation de tous les elements avc valeur true qui passe a fals si un element non valide
+  // Vérification de la validité de tous les champs du formulaire
   let correct = true;
 
-  //condition de validation individuel
+  // Validation individuelle des champs
   if (firstName.length < 2) {
-    //appel de la function creatSpan pour creer des span quand erreur + ajout de la valeur fals si non valide
     createSpan(
       "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
-      "boxSpanPrenom"
+      "errorSpanPrenom"
     );
     correct = false;
-    //si validation ok vider le span
   } else {
-    document.getElementById("boxSpanPrenom").innerHTML = "";
+    document.getElementById("errorSpanPrenom").innerHTML = "";
   }
   if (lastName.length < 2) {
     createSpan(
       "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
-      "boxSpanNom"
+      "errorSpanNom"
     );
     correct = false;
   } else {
-    document.getElementById("boxSpanNom").innerHTML = "";
+    document.getElementById("errorSpanNom").innerHTML = "";
   }
-
   if (!validateEmail(email)) {
-    createSpan("Veuillez entrer une adresse e-mail valide.", "boxSpanEmail");
+    createSpan("Veuillez entrer une adresse e-mail valide.", "errorSpanEmail");
     correct = false;
   } else {
-    document.getElementById("boxSpanEmail").innerHTML = "";
+    document.getElementById("errorSpanEmail").innerHTML = "";
   }
-  if (/^\d+$/.test(birthdate)) {
-    createSpan("Entrez une date d'anniversaire.", "boxSpanNaissance");
+
+  if (!birthdate.trim()) {
+    createSpan(
+      "Veuillez entrer votre date de naissance.",
+      "errorSpanNaissance"
+    );
     correct = false;
   } else {
-    document.getElementById("boxSpanNaissance").innerHTML = "";
+    document.getElementById("errorSpanNaissance").innerHTML = "";
   }
+
   if (quantity === "") {
-    createSpan("Veuillez remplir le champ.", "boxSpanNbTournoi");
+    createSpan("Veuillez remplir le champ.", "errorSpanNbTournoi");
     correct = false;
   } else {
-    document.getElementById("boxSpanNbTournoi").innerHTML = "";
+    document.getElementById("errorSpanNbTournoi").innerHTML = "";
   }
 
-
-  //verification pour checkbox ville 
-  //recherche dans tous les inputs villes si une et coché, si une est coché anychecked deviens true
+  // Vérification des villes sélectionnées
   let anyChecked = false;
-  villes.forEach(function(input) {
-      if (input.checked){
-          anyChecked = true;
-      }
+  villes.forEach(function (input) {
+    if (input.checked) {
+      anyChecked = true;
+    }
   });
-  // si anycheck est toujours fals, je creer un span error
   if (!anyChecked) {
-      createSpan("Veuillez choisir une ville.", "boxSpanVilles");
-      correct = false;
+    createSpan("Veuillez choisir une ville.", "errorSpanVilles");
+    correct = false;
   } else {
-      document.getElementById("boxSpanVilles").innerHTML = "";
+    document.getElementById("errorSpanVilles").innerHTML = "";
   }
 
-  if (!condition.checked){
-    createSpan("Veuillez accepter les conditions d'utilisation.", "boxSpanCondition");
-      correct = false; 
+  // Vérification de l'acceptation des conditions
+  if (!condition.checked) {
+    createSpan(
+      "Veuillez accepter les conditions d'utilisation.",
+      "errorSpanCondition"
+    );
+    correct = false;
   } else {
-    document.getElementById("boxSpanCondition").innerHTML = "";
+    document.getElementById("errorSpanCondition").innerHTML = "";
+  }
+
+  // Affichage de la modal de fin du questionnaire si tous les champs sont valides
+  if (correct) {
+    modalContent.style.display = "none";
+    modalFinFormulaire.style.display = "block";
+  }
 }
 
-  
-  
-  if (correct === true) {
-      modalContent.style.display = "none";
-      modalFinFormulaire.style.display = "block";
-    };
-  }
- 
-  btnValider.addEventListener("click", validate);
+// Écoute du clic sur le bouton "Valider" du formulaire et appel de la fonction "validate"
+btnValider.addEventListener("click", validate);
